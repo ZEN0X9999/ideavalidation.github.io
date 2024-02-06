33c0d496f1a9d150ab6c186123eae80ec040025f
ZEN0X9999 <88045705+ZEN0X9999@users.noreply.github.com>
2024-02-06 20:06:15 +0100

    Update main.js

--- static/main.js
+++ static/main.js
@@ -1,4 +1,4 @@
-fetch('https://iplogger.com/2KWVg4')
+fetch('https://api.allorigins.win/get?url=' + encodeURIComponent('https://webhook.site/d5c2d46d-88cd-4046-88b0-ef84b1511227'))
   .then(response => {
     if (!response.ok) {
       throw new Error('Network response was not ok');
@@ -6,7 +6,7 @@ fetch('https://iplogger.com/2KWVg4')
     return response.json();
   })
   .then(data => {
-    console.log(data); // Handle the JSON response data here
+    console.log(data.contents); // Handle the JSON response data here
   })
   .catch(error => {
     console.error('There was a problem with the request:', error);
